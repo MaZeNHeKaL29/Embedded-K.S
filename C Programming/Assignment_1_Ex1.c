41ce0f644f84ce95c205fdeed0a3a584013214fc
Mazen Hekal <mnhekal29@gmail.com>
2023-07-16 05:47:07 +0300

    Assignment 1 EX_1

---
+++ C Programming/Assignment_1_Ex1.c
@@ -0,0 +1,16 @@
+/*
+ ============================================================================
+ Name        : Assignment_1_Ex1.c
+ Author      : Mazen Hekal
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	printf("C Programming");
+}
